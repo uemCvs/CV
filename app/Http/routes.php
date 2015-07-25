4d821170d4a366e8d9b928daba68a144c2c105af
Eunice <muzimeunice95@gmail.com>
2015-07-25 16:05:40 +0200

    login obrigatorio

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -22,29 +22,6 @@ Route::controllers([
 
 //Route::resource('estudantes', 'EstudanteController');
 
-
-//Estudante
-Route::post('/estudantes','EstudanteController@store');
-Route::get('/estudantes', 'EstudanteController@index');
-Route::get('/estudantes/create', 'EstudanteController@create');
-Route::put('/estudantes/{id}',['as' =>'put_est', 'uses' => 'EstudanteController@update']);
-Route::get('/estudantes/{id}/editar',['as' =>'editar_estudante', 'uses' => 'EstudanteController@edit']);
-Route::get('/estudantes/{id}/',['as' =>'visualizarEstudante', 'uses' => 'EstudanteController@show'])->where(['id' => '[0-9]+']);
-
-
-//Experiencia
-
-Route::get('/experiencias', 'ExperienciaController@index');
-Route::get('/experiencias/create', 'ExperienciaController@create');
-Route::get('/experiencias',['as' =>'editar_experiencia', 'uses' => 'ExperienciaController@edit']);
-Route::put('/experiencias/{id}',['as' =>'put_exp', 'uses' => 'ExperienciaController@update']);
-Route::get('/experiencias/{id}/',['as' =>'visualizarExperiencia', 'uses' => 'ExperienciaController@show'])->where(['id' => '[0-9]+']);
-
-//Qualificacao
-Route::get ('/qualificacao','qualificacaoController@index');
-Route::post ('/qualificacao','qualificacaoController@store');
-
-
 //Idioma
 Route::get ('/idioma','idiomaController@index');
 Route::post ('/idioma','idiomaController@store');
@@ -76,6 +53,18 @@ Route::group(['middleware' => 'auth'], function() {
 	Route::get('/habilitacoes/{id}/',['as' =>'visualizarHabilitacao', 'uses' => 'HabilitacaoController@show'])->where(['id' => '[0-9]+']);
 	Route::put('/habilitacoes/{id}',['as' =>'put_h', 'uses' => 'HabilitacaoController@update']);
 
+
+	//Experiencia
+
+	Route::get('/experiencias', 'ExperienciaController@index');
+	Route::get('/experiencias/create', 'ExperienciaController@create');
+	Route::get('/experiencias',['as' =>'editar_experiencia', 'uses' => 'ExperienciaController@edit']);
+	Route::put('/experiencias/{id}',['as' =>'put_exp', 'uses' => 'ExperienciaController@update']);
+	Route::get('/experiencias/{id}/',['as' =>'visualizarExperiencia', 'uses' => 'ExperienciaController@show'])->where(['id' => '[0-9]+']);
+
+	//Qualificacao
+	Route::get ('/qualificacao','qualificacaoController@index');
+	Route::post ('/qualificacao','qualificacaoController@store');
 
 
 });
